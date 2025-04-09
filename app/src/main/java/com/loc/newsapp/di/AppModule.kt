978c788483ab5de18522b7aaf885262a9f256a27
ppunns <appunn2002@gmail.com>
2025-04-09 13:32:02 +0800

    HomeScreenViewModel

--- app/src/main/java/com/loc/newsapp/di/AppModule.kt
+++ app/src/main/java/com/loc/newsapp/di/AppModule.kt
@@ -5,10 +5,18 @@ import dagger.hilt.components.SingletonComponent
 import android.app.Application
 import com.loc.newsapp.domain.manger.LocalUserManger
 import com.loc.newsapp.data.manger.LocalUserMangerImpl
+import com.loc.newsapp.data.remote.dto.NewsApi
+import com.loc.newsapp.data.repository.NewsRepositoryImpl
+import com.loc.newsapp.domain.repository.NewsRepository
 import dagger.Provides
 import com.loc.newsapp.domain.usecases.app_entry.AppEntryUseCases
 import com.loc.newsapp.domain.usecases.app_entry.ReadAppEntry
 import com.loc.newsapp.domain.usecases.app_entry.SaveAppEntry
+import com.loc.newsapp.domain.usecases.news.GetNews
+import com.loc.newsapp.domain.usecases.news.NewsUsesCase
+import com.loc.newsapp.util.Constants.BASE_URL
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 
@@ -26,4 +34,32 @@ object AppModule {
         readAppEntry = ReadAppEntry(localUserManger),
         saveAppEntry = SaveAppEntry(localUserManger)
     )
+
+
+    @Provides
+    @Singleton
+    fun provideNewsApi():NewsApi{
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(NewsApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNewsRepository(
+        newsApi: NewsApi
+    ):NewsRepository = NewsRepositoryImpl(newsApi)
+
+
+
+    @Provides
+    @Singleton
+    fun provideNewsUseCases(newsRepository: NewsRepository):NewsUsesCase {
+        return NewsUsesCase(
+            getNews = GetNews(newsRepository)
+        )
+    }
+
 }
--- app/src/main/java/com/loc/newsapp/util/Constants.kt
+++ app/src/main/java/com/loc/newsapp/util/Constants.kt
@@ -6,4 +6,6 @@ object Constants {
     const val APP_ENTRY = "app_entry"
 
     const val API_KEY = "1aafe25fc2014838872481e0845a6b17"
+
+    const val BASE_URL = "https://newsapi.org/v2"
 }
